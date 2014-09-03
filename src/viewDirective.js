b27be2b12daa25321daae2d9ffc6508e61286e76
Daan Poron <daan@poron.be>
2014-09-03 11:24:14 +0200

    ui-view has a controller now

--- src/viewDirective.js
+++ src/viewDirective.js
@@ -70,31 +70,114 @@ function $ViewDirective(   $state,   $compile,   $controller,   $injector,   $ui
 
   var directive = {
     restrict: 'ECA',
+    controller: ['$element', function(element) {
+      var isDefault = true;
+
+      this.initial = undefined;
+      this.anchor = undefined;
+      this.parentEl = undefined;
+
+
+      var _cleanupLastView = function() {
+        if (currentEl) {
+          renderer(true).leave(currentEl);
+          currentEl = null;
+        }
+
+        if (currentScope) {
+          currentScope.$destroy();
+          currentScope = null;
+        }
+      };
+
+      var currentScope, currentEl, viewLocals,
+          name      = attrs[directive.name] || attrs.name || '',
+          onloadExp = attrs.onload || '',
+          autoscrollExp = attrs.autoscroll,
+          renderer  = getRenderer(element, attrs, $scope);
+
+      if (name.indexOf('@') < 0) {
+        name = name + '@' + (inherited ? inherited.state.name : ''); 
+      }
+
+      var view = { 
+        'name': name, 
+        'state': null 
+      };
+
+      this.updateView = function(shouldAnimate) {
+        var locals = $state.$current && $state.$current.locals[name];
+
+        if (isDefault) {
+          isDefault = false;
+          element.replaceWith(this.anchor);
+        }
+
+        if (!locals) {
+          _cleanupLastView();
+          currentEl = element.clone();
+          currentEl.html(this.initial);
+          renderer(shouldAnimate).enter(currentEl, this.parentEl, this.anchor);
+
+          currentScope = $scope.$new();
+          $compile(currentEl.contents())(currentScope);
+          return;
+        }
+
+        if (locals === viewLocals) return; // nothing to do
+
+        _cleanupLastView();
+
+        currentEl = element.clone();
+        currentEl.html(locals.$template ? locals.$template : this.initial);
+        renderer(true).enter(currentEl, this.parentEl, this.anchor);
+
+        currentEl.data('$uiView', view);
+
+        viewLocals = locals;
+        view.state = locals.$$state;
+
+        var link = $compile(currentEl.contents());
+
+        currentScope = $scope.$new();
+
+        if (locals.$$controller) {
+          locals.$scope = currentScope;
+          var controller = $controller(locals.$$controller, locals);
+          currentEl.children().data('$ngControllerController', controller);
+        }
+
+        link(currentScope);
+
+        currentScope.$emit('$viewContentLoaded');
+        if (onloadExp) currentScope.$eval(onloadExp);
+
+        if (!angular.isDefined(autoscrollExp) || !autoscrollExp || $scope.$eval(autoscrollExp)) {
+          $uiViewScroll(currentEl);
+        }
+      };
+
+    }],
     compile: function (element, attrs) {
       var initial   = element.html(),
-          isDefault = true,
           anchor    = angular.element($document[0].createComment(' ui-view-anchor ')),
           parentEl  = element.parent();
 
       element.prepend(anchor);
 
-      return function ($scope) {
+      return function ($scope, element, attrs, ctrl) {
+
+        ctrl.initial = initial;
+        ctrl.anchor = anchor;
+        ctrl.parentEl = parentEl;
+
         var inherited = parentEl.inheritedData('$uiView');
-
-        var currentScope, currentEl, viewLocals,
-            name      = attrs[directive.name] || attrs.name || '',
-            onloadExp = attrs.onload || '',
-            autoscrollExp = attrs.autoscroll,
-            renderer  = getRenderer(element, attrs, $scope);
-
-        if (name.indexOf('@') < 0) name = name + '@' + (inherited ? inherited.state.name : '');
-        var view = { name: name, state: null };
 
         var eventHook = function () {
           if (viewIsUpdating) return;
           viewIsUpdating = true;
 
-          try { updateView(true); } catch (e) {
+          try { ctrl.updateView(true); } catch (e) {
             viewIsUpdating = false;
             throw e;
           }
@@ -104,71 +187,7 @@ function $ViewDirective(   $state,   $compile,   $controller,   $injector,   $ui
         $scope.$on('$stateChangeSuccess', eventHook);
         $scope.$on('$viewContentLoading', eventHook);
 
-        updateView(false);
-
-        function cleanupLastView() {
-          if (currentEl) {
-            renderer(true).leave(currentEl);
-            currentEl = null;
-          }
-
-          if (currentScope) {
-            currentScope.$destroy();
-            currentScope = null;
-          }
-        }
-
-        function updateView(shouldAnimate) {
-          var locals = $state.$current && $state.$current.locals[name];
-
-          if (isDefault) {
-            isDefault = false;
-            element.replaceWith(anchor);
-          }
-
-          if (!locals) {
-            cleanupLastView();
-            currentEl = element.clone();
-            currentEl.html(initial);
-            renderer(shouldAnimate).enter(currentEl, parentEl, anchor);
-
-            currentScope = $scope.$new();
-            $compile(currentEl.contents())(currentScope);
-            return;
-          }
-
-          if (locals === viewLocals) return; // nothing to do
-
-          cleanupLastView();
-
-          currentEl = element.clone();
-          currentEl.html(locals.$template ? locals.$template : initial);
-          renderer(true).enter(currentEl, parentEl, anchor);
-
-          currentEl.data('$uiView', view);
-
-          viewLocals = locals;
-          view.state = locals.$$state;
-
-          var link = $compile(currentEl.contents());
-
-          currentScope = $scope.$new();
-
-          if (locals.$$controller) {
-            locals.$scope = currentScope;
-            var controller = $controller(locals.$$controller, locals);
-            currentEl.children().data('$ngControllerController', controller);
-          }
-
-          link(currentScope);
-
-          currentScope.$emit('$viewContentLoaded');
-          if (onloadExp) currentScope.$eval(onloadExp);
-
-          if (!angular.isDefined(autoscrollExp) || !autoscrollExp || $scope.$eval(autoscrollExp)) {
-            $uiViewScroll(currentEl);
-          }
-        }
+        ctrl.updateView(false);
       };
     }
   };
